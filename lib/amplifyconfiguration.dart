7eb2190364931dcf0621897a1eb1dc679050eb88
and2long <and2long@gmail.com>
2021-05-15 12:09:22 +0800

    初次提交

---
+++ lib/amplifyconfiguration.dart
@@ -0,0 +1,17 @@
+const amplifyconfig = ''' {
+    "UserAgent": "aws-amplify-cli/2.0",
+    "Version": "1.0",
+    "api": {
+        "plugins": {
+            "awsAPIPlugin": {
+                "amplifytest": {
+                    "endpointType": "GraphQL",
+                    "endpoint": "https://zavzx6f7gvgzphy3atjqc2v3e4.appsync-api.ap-northeast-1.amazonaws.com/graphql",
+                    "region": "ap-northeast-1",
+                    "authorizationType": "API_KEY",
+                    "apiKey": "da2-uhtwu4jtk5hzthcwkhlu5uuhfi"
+                }
+            }
+        }
+    }
+}''';
